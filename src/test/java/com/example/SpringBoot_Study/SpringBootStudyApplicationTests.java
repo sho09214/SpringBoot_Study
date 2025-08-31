c2ed88663f985d8f671c57aaabe731b5917911a7
sho09214 <motohashi.sho@gmail.com>
2025-08-31 15:59:39 +0900

    create project

---
+++ src/test/java/com/example/SpringBoot_Study/SpringBootStudyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.SpringBoot_Study;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootStudyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
