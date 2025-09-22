c3ccd911d7748ccd6bbba5e79fef422fc938bed4
sho09214 <motohashi.sho@gmail.com>
2025-09-22 20:44:48 +0900

    ソート機能 追加

--- src/main/java/com/example/SpringBoot_Study/service/PostService.java
+++ src/main/java/com/example/SpringBoot_Study/service/PostService.java
@@ -5,6 +5,7 @@ import com.example.SpringBoot_Study.model.Post;
 import com.example.SpringBoot_Study.model.User;
 import com.example.SpringBoot_Study.repository.PostRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -24,8 +25,18 @@ public class PostService {
         return repository.save(post);
     }
 
-    public List<Post> findAll() {
-        return repository.findAll();
+    public List<Post> findAll(String sortBy, String sortOrder) {
+        //Sort.Orderを使ってソート順を設定
+        Sort.Order order;
+        if (sortOrder.equals("asc")) {
+            order = new Sort.Order(Sort.Direction.ASC, sortBy);
+        } else {
+            order = new Sort.Order(Sort.Direction.DESC, sortBy);
+        }
+
+        //Sortオブジェクト作成
+        Sort sort = Sort.by(order);
+        return repository.findAll(sort);
     }
 
     public Optional<Post> findById(Long id) {
@@ -47,15 +58,26 @@ public class PostService {
         return true;
     }
 
-    public List<Post> searchPosts(String keyword, String matchType) {
+    public List<Post> searchPosts(String keyword, String matchType, String sortBy, String sortOrder) {
+        //Sort.Orderを使ってソート順を設定
+        Sort.Order order;
+        if (sortOrder.equals("asc")) {
+            order = new Sort.Order(Sort.Direction.ASC, sortBy);
+        } else {
+            order = new Sort.Order(Sort.Direction.DESC, sortBy);
+        }
+
+        //Sortオブジェクト作成
+        Sort sort = Sort.by(order);
+
         switch (matchType) {
             case "startswith":
-                return repository.findByTitleStartingWithOrContainingStartingWith(keyword, keyword);
+                return repository.findByTitleStartingWithOrContainingStartingWith(keyword, keyword, sort);
             case "endswith":
-                return repository.findByTitleEndingWithOrContainingEndingWith(keyword, keyword);
+                return repository.findByTitleEndingWithOrContainingEndingWith(keyword, keyword, sort);
             case "contains":
             default:
-                return repository.findByTitleContainingOrContentContaining(keyword, keyword);
+                return repository.findByTitleContainingOrContentContaining(keyword, keyword, sort);
         }
     }
 }
